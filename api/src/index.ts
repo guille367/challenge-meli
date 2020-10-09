d376ccc38c75492db502e3aa6126789c607826d2
gponce <guillermo.ponce@almundo.com>
2020-10-08 23:53:19 -0300

    configuracion de docker

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
-import './LoadEnv'; // Must be the first import
-import app from '@server';
-import logger from '@shared/Logger';
+import "./LoadEnv"; // Must be the first import
+import app from "@server";
+import logger from "@shared/Logger";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Start the server
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.API_PORT || 3000);
 app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+  logger.info("Express server started on port: " + port);
 });
