9ac1bf2c69a2dc13841da1398e9ae222db1c2c48
gponce <guillermo.ponce@almundo.com>
2020-10-11 19:53:27 -0300

    rutas configuradas

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Search from "./Search";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Results from "./Results";
+import Detail from "./Detail";
 
 function App() {
   return (
@@ -10,7 +13,18 @@ function App() {
         </div>
       </div>
       <div className="grid grid-cols-12 gap-12">
-        <div className="col-start-2 col-span-10"></div>
+        <div className="col-start-2 col-span-10">
+          <Router>
+            <Switch>
+              <Route path="/items/:id">
+                <Detail />
+              </Route>
+              <Route path="/items">
+                <Results />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
       </div>
     </>
   );
